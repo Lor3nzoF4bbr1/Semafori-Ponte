1ffced208228842bebd1b205c525561a3f0fa590
Lor3nzoF4bbri1 <lorenzo.fabbri01.stud@ispascalcomandini.it>
2021-02-14 18:32:47 +0100

    Step 1: aggiunte immagini, creati thread di movimento

---
+++ Semafori-Ponte/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Semafori_Ponte
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyThread tr1 = new MyThread();
+
+            Thread t1 = new Thread(new ThreadStart(tr1.MovimentoDestraSinistra));
+            Thread t2 = new Thread(new ThreadStart(tr1.MovimentoSinistraDestra));
+
+            t1.Start();
+            t2.Start();
+        }
+
+        public class MyThread
+        {
+            public void MovimentoDestraSinistra()
+            {
+
+            }
+
+            public void MovimentoSinistraDestra()
+            {
+
+            }
+        }
+    }
+}
