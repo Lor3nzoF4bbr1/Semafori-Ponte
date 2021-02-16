53f82aefaccbff266c90e1a247414b5bff4c66e6
Lor3nzoF4bbri1 <lorenzo.fabbri01.stud@ispascalcomandini.it>
2021-02-16 15:30:18 +0100

    Step 3: possibilità di scegliere il numero di macchine per lato, aggiornati i movimenti

--- Semafori-Ponte/MainWindow.xaml.cs
+++ Semafori-Ponte/MainWindow.xaml.cs
@@ -26,6 +26,9 @@ namespace Semafori_Ponte
         readonly Uri uriMacchinaSinistra = new Uri("Macchina-sinistra.png", UriKind.Relative);
         static int posMacchinaSinistra = 624;
 
+       private int macchineSinistra;
+       private int macchineDestra;
+
         private static object x = new object();
 
         public MainWindow()
@@ -37,27 +40,32 @@ namespace Semafori_Ponte
             ImageSource imgSx = new BitmapImage(uriMacchinaDestra);
             imgMacchinaDestra.Source = imgSx;
 
-            Thread t1 = new Thread(new ThreadStart(MovimentoDestraSinistra));
-            Thread t2 = new Thread(new ThreadStart(MovimentoSinistraDestra));
-
-            t1.Start();
-            t2.Start();
+            imgMacchinaDestra.Visibility = Visibility.Hidden;
+            imgMacchinaSinistra.Visibility = Visibility.Hidden;
         }
 
         public void MovimentoDestraSinistra()
         {
             lock (x)
             {
-                while (posMacchinaDestra > -463)
+                for (int i = 0; i < macchineDestra; i++)
                 {
-                    posMacchinaDestra -= 2;
-
-                    Thread.Sleep(TimeSpan.FromMilliseconds(7));
-
-                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    while (posMacchinaDestra > -463)
                     {
-                        imgMacchinaDestra.Margin = new Thickness(posMacchinaDestra, 55, 0, 231);
-                    }));
+                        posMacchinaDestra -= 2;
+
+                        Thread.Sleep(TimeSpan.FromMilliseconds(7));
+
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            imgMacchinaDestra.Margin = new Thickness(posMacchinaDestra, 55, 0, 231);
+                        }));
+                    }
+
+                    if (posMacchinaDestra <= -463)
+                    {
+                        posMacchinaDestra = 634;
+                    }
                 }
             }
         }
@@ -66,17 +74,54 @@ namespace Semafori_Ponte
         {
             lock (x)
             {
-                while (posMacchinaSinistra > -463)
+                for(int i = 0; i < macchineSinistra; i++)
                 {
-                    posMacchinaSinistra -= 2;
-
-                    Thread.Sleep(TimeSpan.FromMilliseconds(7));
-
-                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    while (posMacchinaSinistra > -463)
                     {
-                        imgMacchinaSinistra.Margin = new Thickness(0, 127, posMacchinaSinistra, 169);
-                    }));
+                        posMacchinaSinistra -= 2;
+
+                        Thread.Sleep(TimeSpan.FromMilliseconds(7));
+
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            imgMacchinaSinistra.Margin = new Thickness(0, 127, posMacchinaSinistra, 169);
+                        }));
+                    }
+
+                    if (posMacchinaSinistra <= -463)
+                    {
+                        posMacchinaSinistra = 624;
+                    }
                 }
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                macchineSinistra = int.Parse(txtNMacchineSinistra.Text);
+                macchineDestra = int.Parse(txtNMacchineDestra.Text);
+
+                if (macchineDestra > 0)
+                {
+                    imgMacchinaDestra.Visibility = Visibility.Visible;
+                }
+
+                if (macchineSinistra> 0)
+                {
+                    imgMacchinaSinistra.Visibility = Visibility.Visible;
+                }
+
+                Thread t1 = new Thread(new ThreadStart(MovimentoDestraSinistra));
+                Thread t2 = new Thread(new ThreadStart(MovimentoSinistraDestra));
+
+                t1.Start();
+                t2.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
     }
